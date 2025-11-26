75737b94bfcb7b00dedf2818130e1420bd39af98
Adams <adamsdexter3@gmail.com>
2025-11-26 16:14:15 +0100

    Fix Vercel ESM configuration

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 // Handler pour Vercel Serverless Functions à la racine
 import app from '../backend/index.js';
 
+// Vercel attend un handler qui reçoit (req, res)
+// L'app Express est déjà configurée pour gérer les requêtes
 export default app;
 
